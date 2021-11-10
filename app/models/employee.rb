ea5dbbc8a8bd1b96ba111ea28b98761c86f044e5
someshsuryavanshi <someshyuvasoft334@gmail.com>
2021-11-10 15:21:55 +0530

    js_change

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,6 @@ class Employee < ApplicationRecord
 	  has_one_attached :files
 	  has_one_attached :image
 
-      validates :name,:email,:date_of_birth,:gender,:Employe_type,:Technology ,:description,:skills, presence: true
+      validates :Employe_type,:skills, presence: true
 	 
 end
